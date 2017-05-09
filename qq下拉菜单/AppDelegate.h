bca93b2e5e4571558918fc5a1b822adb703de738
jiafa <jiafa@jingkeciqinwang.local>
2017-05-09 14:18:21 +0800

    Initial Commit

---
+++ qq下拉菜单/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qq下拉菜单
+//
+//  Created by 简而言之 on 2017/5/9.
+//  Copyright © 2017年 jiafa.apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
